060ac9cc17e9594f99e671c18673ec0fbe9e47b7
pedroMontandon <pedrobarbosa89@gmail.com>
2023-06-29 10:12:50 -0300

    Tests coverage on 90%

--- tests/integration/orders/create.test.ts
+++ tests/integration/orders/create.test.ts
@@ -6,6 +6,7 @@ import OrderModel from '../../../src/database/models/order.model';
 import jwtUtils from '../../../src/utils/jwt.utils';
 import ordersService from '../../../src/service/orders.service';
 import UserModel from '../../../src/database/models/user.model';
+import ProductModel from '../../../src/database/models/product.model';
 
 chai.use(chaiHttp);
 
@@ -25,21 +26,27 @@ describe('POST /orders', function () {
     expect(response.body).to.deep.eq({ message: 'Invalid token' });
   })
   it('Failing to create an order without an userId', async function(){
+    const tokenModelMocked = OrderModel.build({ id: 1, userId: 3 })
     sinon.stub(jwtUtils, 'verify').returns({id: 1, username: 'Chapolin'})
+    sinon.stub(OrderModel, 'findOne').resolves(tokenModelMocked);
     const response = await chai.request(app).post('/orders')
       .send({ productIds: [2] }).set('Authorization', 'validToken')
     expect(response.status).to.eq(400);
     expect(response.body).to.deep.eq({ message: '"userId" is required'});
   })
   it('Failing to create an order using a typed string "userId"', async function() {
+    const tokenModelMocked = OrderModel.build({ id: 1, userId: 3 })
     sinon.stub(jwtUtils, 'verify').returns({id: 1, username: 'Chapolin'})
+    sinon.stub(OrderModel, 'findOne').resolves(tokenModelMocked);
     const response = await chai.request(app).post('/orders')
       .send({ userId: 'oi!', productIds: [2] }).set('Authorization', 'validToken')
     expect(response.status).to.eq(422);
     expect(response.body).to.deep.eq({ message: '"userId" must be a number'});
   })
   it('Failing to create an order using a nonexistent "userId"', async function(){
+    const tokenModelMocked = OrderModel.build({ id: 1, userId: 3 })
     sinon.stub(jwtUtils, 'verify').returns({id: 1, username: 'Chapolin'})
+    sinon.stub(OrderModel, 'findOne').resolves(tokenModelMocked);
     sinon.stub(UserModel, 'findOne').resolves(null)
     const response = await chai.request(app).post('/orders')
       .send({ userId: 1, productIds: [2] }).set('Authorization', 'validToken')
@@ -47,24 +54,49 @@ describe('POST /orders', function () {
     expect(response.body).to.deep.eq({ message: '"userId" not found'});
   })
   it('Failing to create an order without a productId', async function() {
+    const tokenModelMocked = OrderModel.build({ id: 1, userId: 1 })
+    const userMockModel = UserModel.build({username: 'Chapolin', vocation: 'herói', level: 2, password: 'oi'})
     sinon.stub(jwtUtils, 'verify').returns({id: 1, username: 'Chapolin'})
+    sinon.stub(OrderModel, 'findOne').resolves(tokenModelMocked);
+    sinon.stub(UserModel, 'findOne').resolves(userMockModel)
     const response = await chai.request(app).post('/orders')
       .send({ userId: 1 }).set('Authorization', 'validToken')
     expect(response.status).to.eq(400);
     expect(response.body).to.deep.eq({ message: '"productIds" is required'});
   })
   it('Failing to create an order using a typed string in the array productId', async function() {
+    const tokenModelMocked = OrderModel.build({ id: 1, userId: 1 })
+    const userMockModel = UserModel.build({username: 'Chapolin', vocation: 'herói', level: 2, password: 'oi'})
+    sinon.stub(UserModel, 'findOne').resolves(userMockModel)
     sinon.stub(jwtUtils, 'verify').returns({id: 1, username: 'Chapolin'})
+    sinon.stub(OrderModel, 'findOne').resolves(tokenModelMocked);
     const response = await chai.request(app).post('/orders')
       .send({ userId: 1, productIds: [] }).set('Authorization', 'validToken')
     expect(response.status).to.eq(422);
     expect(response.body).to.deep.eq({ message: '"productIds" must include only numbers'});
   })
   it('Failing to create an order passing a number for productIds', async function() {
+    const tokenModelMocked = OrderModel.build({ id: 1, userId: 1 })
+    const userMockModel = UserModel.build({username: 'Chapolin', vocation: 'herói', level: 2, password: 'oi'})
+    sinon.stub(UserModel, 'findOne').resolves(userMockModel)
     sinon.stub(jwtUtils, 'verify').returns({id: 1, username: 'Chapolin'})
+    sinon.stub(OrderModel, 'findOne').resolves(tokenModelMocked);
     const response = await chai.request(app).post('/orders')
       .send({ userId: 1, productIds: 1 }).set('Authorization', 'validToken')
     expect(response.status).to.eq(422);
     expect(response.body).to.deep.eq({ message: '"productIds" must be an array'});
   })
+  it('Creating an order', async function () {
+    const tokenModelMocked = OrderModel.build({ id: 1, userId: 1 })
+    const userMockModel = UserModel.build({username: 'Chapolin', vocation: 'herói', level: 2, password: 'oi'})
+    sinon.stub(jwtUtils, 'verify').returns({id: 1, username: 'Chapolin'});
+    sinon.stub(UserModel, 'findOne').resolves(userMockModel);
+    sinon.stub(OrderModel, 'findOne').resolves(tokenModelMocked);
+    sinon.stub(OrderModel, 'create').resolves();
+    sinon.stub(ProductModel, 'update').resolves();
+    const response = await chai.request(app).post('/orders')
+      .send({ userId: 1, productIds: [2] }).set('Authorization', 'validToken');
+    expect(response.status).to.eq(201);
+    expect(response.body).to.deep.eq({ userId: 1, productIds: [2] });
+  });
 });
